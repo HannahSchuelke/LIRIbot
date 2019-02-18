42468b0443c1d703febcdc94a7ab6b5634e049b2
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-02-18 16:21:37 -0600

    linking

--- keys.js
+++ keys.js
@@ -1,6 +1,21 @@
+// tests
 console.log('this is loaded');
 
+// API Keys:
+
+// spotify
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+// OMDb API:
+var axios = require("axios");
+
+// We then run the request with axios module on a URL with a JSON
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // Then we print out the imdbRating
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
